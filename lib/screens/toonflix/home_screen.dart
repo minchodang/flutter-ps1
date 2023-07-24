b7ef654cfcdf26d08a97172e2491620583ea5002
minsuKang <minsu910725@gmail.com>
2023-07-24 09:19:50 +0900

    Feat: flutter-builder

--- lib/screens/toonflix/home_screen.dart
+++ lib/screens/toonflix/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/modules/webtoon_model.dart';
+import 'package:toonflix/services/api_services.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -9,14 +13,24 @@ class HomeScreen extends StatelessWidget {
       backgroundColor: Colors.white,
       appBar: AppBar(
         elevation: 2,
-        title: const Text(
-          "오늘의 웹툰",
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.w600),
-        ),
         backgroundColor: Colors.white,
         foregroundColor: Colors.green,
+        title: const Text(
+          "어늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+          ),
+        ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const Text("There is data!");
+          }
+          return const Text('Loading....');
+        },
       ),
     );
   }
 }
-
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -4,18 +4,20 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/modules/webtoon_model.dart';
 
 class ApiService {
-  List<WebtoonModel> webtoonInstances = [];
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
-  Future<List<WebtoonModel>> getTodayToons() async {
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
+
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse('$baseUrl/$today');
     final response = await http.get(url);
     if (response.statusCode == 200) {
-      final List<dynamic> webtoons = jsonDecode(response.body);
+      final webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
-
       return webtoonInstances;
     }
     throw Error();
