c483e0344683a4181971db1ddda6ff7d14b76c41
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-07-25 09:21:56 +0900

    Feat: flutter-builder (#28)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/toonflix/home_screen.dart';
-import 'package:toonflix/services/api_services.dart';
 
 void main() {
-  ApiService().getTodayToons();
   runApp(const App());
 }
 
@@ -12,7 +10,7 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomeScreen(),
     );
   }
