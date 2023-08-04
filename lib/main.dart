88ec08f8dcfa8fc59f233b5ca5c3ef46dca5967d
minsuKang <90169703+minchodang@users.noreply.github.com>
2023-08-04 09:32:12 +0900

    Feat: webtoon-card (#30)

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/toonflix/home_screen.dart';
 
@@ -13,5 +15,14 @@ class App extends StatelessWidget {
     return MaterialApp(
       home: HomeScreen(),
     );
+  }
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..userAgent =
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36';
   }
 }
