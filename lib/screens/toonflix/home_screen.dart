2f5c32ab512f53ca39e3f8625a6b03668dc3d572
minsuKang <minsu910725@gmail.com>
2023-08-05 11:16:18 +0900

    Feat: detail Page

--- lib/screens/toonflix/home_screen.dart
+++ lib/screens/toonflix/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix/services/api_services.dart';
+import 'package:toonflix/widget/webtoon_widget.dart';
 
 import '../../modules/webtoon_model.dart';
 
@@ -51,34 +52,8 @@ class HomeScreen extends StatelessWidget {
       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 10),
       itemBuilder: (context, index) {
         var webtoon = snapshot.data![index];
-        return Column(children: [
-          Container(
-            width: 250,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 15,
-                      offset: const Offset(10, 10),
-                      color: Colors.black.withOpacity(0.5))
-                ]),
-            child: Image.network(
-              webtoon.thumb,
-              headers: const {
-                "User-Agent":
-                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-              },
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Text(
-            webtoon.title,
-            style: const TextStyle(fontSize: 22),
-          ),
-        ]);
+        return Webtoon(
+            title: webtoon.title, thumb: webtoon.thumb, id: webtoon.id);
       },
       separatorBuilder: (context, index) => const SizedBox(
         width: 40,
